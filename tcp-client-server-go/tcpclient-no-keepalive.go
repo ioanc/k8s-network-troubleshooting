c54c0375f4e0a0896d59eb39a7a8b876b96b1d12
root <root@test2.home.arpa>
2023-07-03 16:42:58 +0000

    fix code

--- tcp-client-server-go/tcpclient-no-keepalive.go
+++ tcp-client-server-go/tcpclient-no-keepalive.go
@@ -1,51 +1,51 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "net"
-        "os"
-        "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
 )
 
 func main() {
-        arguments := os.Args
-        if len(arguments) == 1 {
-                fmt.Println("Please provide host:port.")
-                return
-        }
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide host:port.")
+		return
+	}
 
-                // Resolve TCP Address
-                CONNECT := arguments[1]
-                addr, err := net.ResolveTCPAddr("tcp", CONNECT)
-                if err != nil {
-                        fmt.Printf("Unable to resolve IP")
-                }
+	// Resolve TCP Address
+	CONNECT := arguments[1]
+	addr, err := net.ResolveTCPAddr("tcp", CONNECT)
+	if err != nil {
+		fmt.Printf("Unable to resolve IP")
+	}
 
-        c, err := net.DialTCP("tcp", nil, addr)
-          if err != nil {
-                fmt.Println(err)
-                return
-        }
+	c, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-                // Enable Keepalives
-                err = c.SetKeepAlive(false)
-                if err != nil {
-                        fmt.Printf("Unable to set keepalive - %s", err)
-                }
+	// Enable Keepalives
+	err = c.SetKeepAlive(false)
+	if err != nil {
+		fmt.Printf("Unable to set keepalive - %s", err)
+	}
 
-        for {
-                reader := bufio.NewReader(os.Stdin)
-                reader := bufio.NewReader(os.Stdin)
-                fmt.Print(">> ")
-                text, _ := reader.ReadString('\n')
-                fmt.Fprintf(c, text+"\n")
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		//reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(c, text+"\n")
 
-                message, _ := bufio.NewReader(c).ReadString('\n')
-                fmt.Print("->: " + message)
-                if strings.TrimSpace(string(text)) == "STOP" {
-                        fmt.Println("TCP client exiting...")
-                        return
-                }
-        }
-}        
+		message, _ := bufio.NewReader(c).ReadString('\n')
+		fmt.Print("->: " + message)
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
+	}
+}
--- tcp-client-server-go/tcpserver-no-keepalive.go
+++ tcp-client-server-go/tcpserver-no-keepalive.go
@@ -1,74 +1,73 @@
 package main
 
 import (
-        "bufio"
-        "fmt"
-        "net"
-        "os"
-        "strings"
-        "time"
-        "strconv"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-        arguments := os.Args
-        if len(arguments) == 1 {
-                fmt.Println("Please provide 'address port delay'")
-                return
-        }
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide 'address port delay'")
+		return
+	}
 
-                // Resolve TCP Address
-                PORT := arguments[1] + ":" + arguments[2]
-                addr, err := net.ResolveTCPAddr("tcp", PORT)
-                if err != nil {
-                        fmt.Printf("Unable to resolve IP")
-                }
-          l, err := net.ListenTCP("tcp", addr)
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-        defer l.Close()
+	// Resolve TCP Address
+	PORT := arguments[1] + ":" + arguments[2]
+	addr, err := net.ResolveTCPAddr("tcp", PORT)
+	if err != nil {
+		fmt.Printf("Unable to resolve IP")
+	}
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
 
-        c, err := l.AcceptTCP()
-        if err != nil {
-                fmt.Println(err)
-                return
-        }
-                // Enable / Disable Keepalives
-                err = c.SetKeepAlive(false)
-                if err != nil {
-                        fmt.Printf("Unable to set keepalive - %s", err)
-                }
-        for {
-                netData, err := bufio.NewReader(c).ReadString('\n')
-                if err != nil {
-                        fmt.Println(err)
-                        return
-                }
-        
-                if strings.TrimSpace(string(netData)) == "STOP" {
-                        fmt.Println("Exiting TCP server!")
-                        return
-                }
-                fmt.Print("-> ", string(netData))
+	c, err := l.AcceptTCP()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Enable / Disable Keepalives
+	err = c.SetKeepAlive(false)
+	if err != nil {
+		fmt.Printf("Unable to set keepalive - %s", err)
+	}
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
-                i, err := strconv.Atoi(arguments[3])
-                   if err != nil {
-                        fmt.Println(err)
-                        return
-                }
-                          }
-                fmt.Println((time.Now()).Format(time.RFC3339 + "\n"))
+		if strings.TrimSpace(string(netData)) == "STOP" {
+			fmt.Println("Exiting TCP server!")
+			return
+		}
+		fmt.Print("-> ", string(netData))
 
-                time.Sleep((time.Duration(i) * time.Second))
+		i, err := strconv.Atoi(arguments[3])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		//}
+		fmt.Println((time.Now()).Format(time.RFC3339 + "\n"))
 
-                fmt.Print("-> replay", "\n")
-                fmt.Println((time.Now()).Format(time.RFC3339 + "\n"))
+		time.Sleep((time.Duration(i) * time.Second))
 
-                t := time.Now()
-                myTime := t.Format(time.RFC3339 + "\n")
-                c.Write([]byte(myTime))
-        }
+		fmt.Print("-> replay", "\n")
+		fmt.Println((time.Now()).Format(time.RFC3339 + "\n"))
+
+		t := time.Now()
+		myTime := t.Format(time.RFC3339 + "\n")
+		c.Write([]byte(myTime))
+	}
 }
-          
