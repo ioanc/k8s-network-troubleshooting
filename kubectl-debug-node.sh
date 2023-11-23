710d43f678bcaa697d2cb010189debc78ad80540
ioanc <ioanc@live.com>
2023-11-23 13:44:50 +0100

    Update kubectl-debug-node.sh

--- kubectl-debug-node.sh
+++ kubectl-debug-node.sh
@@ -2,7 +2,7 @@
 NODE="node-name"
 
 # Image name
-IMAGE="mcr.microsoft.com/oss/nginx/nginx:1.21.6"
+IMAGE="ubuntu:22.04"
 
 # works with ubuntu 22.04
 kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["nsenter", "--all", "--target=1", "--", "su", "-"], "stdin": true, "tty": true, "securityContext": { "privileged": true }}] } }'
