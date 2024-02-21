f2c59ab3b44addc794ca31da6fd595021a73b63c
ioanc <ioanc@live.com>
2024-02-21 17:56:07 +0100

    Update kubectl-debug-node.sh

--- kubectl-debug-node.sh
+++ kubectl-debug-node.sh
@@ -7,7 +7,7 @@
 IMAGE="mcr.microsoft.com/cbl-mariner/base/core:2.0"
 NODE="aks-nodepool1-21777443-vmss000005"
 
-# works with ubuntu 20.04+ and Azure Linux 2+
+# works with AKS node images Ubuntu 22.04+ and Azure Linux
 kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["sh","-xc","tdnf install util-linux -y -q; nsenter --all --target=1 -- bash"], "stdin": true, "tty": true, "securityContext": {"privileged": true }}] } }'
 
 # works with ubuntu - 18.04
