8aadc6928cf6f1afb0768f9919f6bca26b3dd910
ioanc <ioanc@live.com>
2024-02-21 13:43:07 +0100

    Update kubectl-debug-node.sh

--- kubectl-debug-node.sh
+++ kubectl-debug-node.sh
@@ -1,13 +1,10 @@
+# Accessing AKS node with full rights
 # This is using two variables:
 #
 # IMAGE : the container image used to start the pod on the AKS node
 # NODE : the AKS node name
 
-IMAGE="ubuntu:22.04"
+IMAGE="mcr.microsoft.com/cbl-mariner/base/core:2.0"
 NODE="aks-nodepool1-21777443-vmss000005"
 
-# nodes based on Ubuntu 22.04 disk image
-kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["nsenter", "--all", "--target=1", "--", "su", "-"], "stdin": true, "tty": true, "securityContext": { "privileged": true }}] } }'
-
-# nodes based on Ubuntu 18.04 disk image - deprecated since AKS 1.25
-# kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["nsenter", "-m", "-u", "-i", "-n", "-p", "-C", "-r", "-w", "--target=1", "--", "su", "-"], "stdin": true, "tty": true, "securityContext": { "privileged": true }}] } }'
+kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["sh","-xc","tdnf install util-linux -y -q; nsenter --all --target=1 -- bash"], "stdin": true, "tty": true, "securityContext": {"privileged": true }}] } }'
