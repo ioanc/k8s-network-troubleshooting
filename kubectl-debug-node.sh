4b73333de234ab9a7cb8affeb991f9c71af3f361
ioanc <ioanc@live.com>
2024-04-17 11:10:01 +0200

    update kubectl-debug-node.sh

--- kubectl-debug-node.sh
+++ kubectl-debug-node.sh
@@ -4,14 +4,14 @@
 # IMAGE : the container image used to start the pod on the AKS node
 # NODE : the AKS node name
 
-# IMAGE="mcr.microsoft.com/cbl-mariner/base/core:2.0"
 IMAGE='mcr.microsoft.com/mirror/docker/library/busybox:1.35'
 NODE="aks-nodepool1-21777443-vmss000005"
 
 # works with AKS node images Ubuntu 22.04+ and Azure Linux
 kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["sh","-xc","nsenter -m -u -i -n -p -r -w -t 1 -- bash"], "stdin": true, "tty": true, "securityContext": {"privileged": true }}] } }'
 
-#kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["sh","-xc","tdnf install util-linux -y -q; nsenter --all --target=1 -- bash"], "stdin": true, "tty": true, "securityContext": {"privileged": true }}] } }'
 
+IMAGE='mcr.microsoft.com/cbl-mariner/base/core:2.0'
+NODE="aks-nodepool1-21777443-vmss000005"
 # works with ubuntu - 18.04
 kubectl run debug-node-$NODE --restart=Never -it --rm --image overriden --overrides '{"spec": {"hostPID": true,"hostNetwork": true, "nodeSelector": { "kubernetes.io/hostname": "'${NODE:?}'"}, "tolerations": [{"operator": "Exists"}],"containers": [{"name": "nsenter", "image": "'${IMAGE:?}'","command": ["sh","-xc","tdnf install util-linux -y -q; nsenter -m -u -i -n -p -C -r -w --target=1 -- bash"], "stdin": true, "tty": true, "securityContext": { "privileged": true }}] } }'
