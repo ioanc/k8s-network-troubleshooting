66a9321af2b92b53a2ab5273ac2ca8721d87a179
ioanc <ioanc@live.com>
2024-05-31 10:46:09 +0200

    add TIMEOUT variable in kubectl

--- tcpdump-node-local.sh
+++ tcpdump-node-local.sh
@@ -3,4 +3,4 @@
 NODE="aks-1213-NODE_NAME"
 CHUNK="60"
 TIMEOUT="300"
-kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout 300 tcpdump -Z root -i any -G $CHUNK -w $NODE-%M.pcap 'tcp[tcpflags] & (tcp-syn) == (tcp-syn)' ; sleep inf"
+kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -G $CHUNK -w $NODE-%M.pcap 'tcp[tcpflags] & (tcp-syn) == (tcp-syn)' ; sleep inf"
