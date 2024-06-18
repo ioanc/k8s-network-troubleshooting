e8f71b19aaadb9e7804d3c2118442442a45e2237
ioanc <ioanc@live.com>
2024-06-18 14:55:19 +0200

    add pod-to-pod-daemonset-connection-matrix.yaml and minor changes

--- kubectl-debug-node.sh
+++ kubectl-debug-node.sh
@@ -2,7 +2,10 @@
 # This is using two variables:
 #
 # IMAGE : the container image used to start the pod on the AKS node
-# NODE : the AKS node name
+# NODE : the AKS / K8s node name
+# With kubectl v1.30 you can use the following command to access the AKS node with full rights
+# kubectl debug node/<node-name> -it --image=<image-name> --profile=sysadmin
+# https://github.com/kubernetes/kubernetes/pull/119200
 
 IMAGE='mcr.microsoft.com/mirror/docker/library/busybox:1.35'
 NODE="aks-nodepool1-21777443-vmss000005"
