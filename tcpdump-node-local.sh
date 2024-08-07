d02c55b38f75dc52f545117dba92a3bab2b696ef
ioanc <ioanc@live.com>
2024-08-07 15:44:23 +0200

    change capture filter

--- tcpdump-node-local.sh
+++ tcpdump-node-local.sh
@@ -1,6 +1,8 @@
 # Run a tcpdump for $TIMEOUT seconds creating a new file every $CHUNk seconds on an AKS $NODE and save the file on the root folder of node with the name $NODE-%M.pcap
+# Capture files will be rotted after 60 minutes.
+# Capure will be filtered IP address 1.2.3.4
 
 NODE="aks-1213-NODE_NAME"
 CHUNK="60"
 TIMEOUT="300"
-kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -G $CHUNK -w $NODE-%M.pcap 'tcp[tcpflags] & (tcp-syn) == (tcp-syn)' ; sleep inf"
+kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -G $CHUNK -w $NODE-%M.pcap 'host 1.2.3.4' ; sleep inf"
