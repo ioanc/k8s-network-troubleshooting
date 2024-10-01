9a563d04961349e1fc9ee852c463cb8658e8db0a
ioanc <ioanc@live.com>
2024-10-01 17:07:40 +0200

    add 12h rotate with %I

--- tcpdump-node-local.sh
+++ tcpdump-node-local.sh
@@ -1,7 +1,7 @@
 # Run a tcpdump for $TIMEOUT seconds creating a new file every $CHUNk seconds on an AKS $NODE and save the file on the root folder of node with the name $NODE-%M.pcap
 # Capture files will be rotted after 60 minutes.
 # Capure will be filtered IP address 1.2.3.4
-# Files will be rotated after every 12h
+# Files will be rotated after every 12h, using the file name $NODE_%I-%M.pcap - %I - strftime 
 
 NODE="aks-1213-NODE_NAME"
 CHUNK="60"
