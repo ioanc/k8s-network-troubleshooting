25e80d04ccb7e92c7569e5231cc2ea73fba2f56a
ioanc <ioanc@live.com>
2024-11-05 12:31:06 +0100

    Update tcpdump-node-local.sh

--- tcpdump-node-local.sh
+++ tcpdump-node-local.sh
@@ -1,9 +1,11 @@
 # Run a tcpdump for $TIMEOUT seconds creating a new file every $CHUNk seconds on an AKS $NODE and save the file on the root folder of node with the name $NODE-%M.pcap
-# Capture files will be rotted after 60 minutes.
-# Capure will be filtered IP address 1.2.3.4
-# Files will be rotated after every 12h, using the file name $NODE_%I-%M.pcap - %I - strftime 
+# Capture files will be rotted every 60 minutes, using the file name $NODE-%M.pcap
+# Capture files will be rotted every 12 hours, using the file name $NODE_%I-%M.pcap ; check 'man strftime'
+# We capture only the fist 256 bites of the packet; -s 256
+# tcpdump will capture only IP address 1.2.3.4 ; Change the IP address in the filter 
+
 
 NODE="aks-1213-NODE_NAME"
 CHUNK="60"
-TIMEOUT="300"
-kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -G $CHUNK -w $NODE_%I-%M.pcap 'host 1.2.3.4' ; sleep inf"
+TIMEOUT="3600"
+kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -s 256 -G $CHUNK -w $NODE_%I-%M.pcap 'host 1.2.3.4' ; sleep inf"
