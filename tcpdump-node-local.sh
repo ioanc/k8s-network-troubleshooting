62381f1b307fb70d5633cc24bdb8ed0f6b08ecfb
ioanc <ioanc@live.com>
2024-11-06 18:54:03 +0100

    Update tcpdump-node-local.sh

--- tcpdump-node-local.sh
+++ tcpdump-node-local.sh
@@ -5,11 +5,10 @@
 # tcpdump will capture only IP address 1.2.3.4 ; Change the IP address in the filter for the POD IP address running on the node, that you need to capture the traffic.
 # This is just an example and will need to be customised for specific troubleshooting sceanrio! 
 
-# replace with the actual noe name
+# replace with the actual node name;
 NODE="aks-1213-NODE_NAME"
-# repace with the number of seconds you want tcpdump to stop automaticaly; we use here 3600s -> 1 hour.
+# repace with the number of seconds you want tcpdump to stop automaticaly;
 TIMEOUT="3600"
-
-# create a new file every 300 seconds
+# create a new file every 300 seconds;
 CHUNK="300"
 kubectl debug node/$NODE --image mcr.microsoft.com/mirror/docker/library/busybox:1.35 -- sh -xc "chroot /host timeout $TIMEOUT tcpdump -Z root -i any -s 256 -G $CHUNK -w $NODE_%I-%M.pcap 'host 1.2.3.4' ; sleep inf"
